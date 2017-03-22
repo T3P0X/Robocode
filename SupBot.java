769356c555f0549ff6930c7688486572ef5d538a
T3P0X <heyimlars@gmail.com>
2017-03-22 15:39:14 +0100

    nummero cinco

--- SupBot.java
+++ SupBot.java
@@ -13,41 +13,24 @@ public class SupBot extends AdvancedRobot {
     private int movementDirection = 1;
     private int strafeDirection = 1;
 
-    AdvancedEnemy enemy = new AdvancedEnemy();
-    ArrayList<AdvancedEnemy> enemies = new ArrayList<>();
-
-
     public void run() {
 
-        enemy.resetEnemy();
         setAdjustRadarForRobotTurn(true);
+        setAdjustRadarForGunTurn(true);
+        setAdjustGunForRobotTurn(true);
 
         turnRadarRight(360);
         while (true) {
             turnRadarRight(360);
             scan();
-
             execute();
-
         }
         }
-
-    public void onRobotDeath(RobotDeathEvent robot) {
-        // see if the robot we were tracking died
-        if (robot.getName().equals(enemy.getName())) {
-            enemy.resetEnemy();
-        }
-    }
 
     public void onScannedRobot(ScannedRobotEvent robot) {
 //        double moveAmount = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
 //        boolean peek = false;
-        if ( enemy.none() || robot.getDistance() < enemy.getDistance() - 70 ||
-                robot.getName().equals(enemy.getName())) {
 
-            // track him using the NEW update method
-            enemy.updateEnemy(robot , this);
-        }
 
         double radarTurn = getHeadingRadians() + robot.getBearingRadians() - getRadarHeadingRadians();
 
@@ -82,17 +65,14 @@ public class SupBot extends AdvancedRobot {
             back(100);
         }
         setAhead((robot.getDistance() / 4 + 40) * movementDirection);
-        shoot();
 
+
+        
 
             // Track the energy level
             previousEnergy = robot.getEnergy();
             execute();
         }
-
-
-
-
 
 
     private void InfoRobot() {
@@ -112,56 +92,8 @@ public class SupBot extends AdvancedRobot {
         System.out.println("=======================");
     }
 
-    private boolean enemyIsUnique(String name) {
-        boolean ok = true;
-        for (Enemy enemy : enemies) {
-            if (name.equals(enemy.getName())) {
-                ok = false;
-            }
-        }
-        return ok;
     }
 
-    public void shoot(){
-        if (enemy.none()) {
-
-            return;
-        }
-        double turn = getHeading() - getGunHeading() + enemy.getBearing();
-        // normalize the turn to take the shortest path there
-        setTurnGunRight(normalizeBearing(turn));
-        // shoot!
-        setFire(Math.min(400 / enemy.getDistance(), 3));
-    }
-
-    private double normalizeBearing(double angle) {
-        while (angle >  180) angle -= 360;
-        while (angle < -180) angle += 360;
-        return angle;
-    }
-    private double absoluteBearing(double x1, double y1, double x2, double y2) {
-        double xo = x2-x1;
-        double yo = y2-y1;
-        double hyp = Point2D.distance(x1, y1, x2, y2);
-        double arcSin = Math.toDegrees(Math.asin(xo / hyp));
-        double bearing = 0;
-
-        if (xo > 0 && yo > 0) {
-            bearing = arcSin;
-        } else if (xo < 0 && yo > 0) {
-            bearing = 360 + arcSin;
-        } else if (xo > 0 && yo < 0) {
-            bearing = 180 - arcSin;
-        } else if (xo < 0 && yo < 0) {
-            bearing = 180 - arcSin;
-        }
-
-        return bearing;
-    }
-
-
-
-}
 
 
 
